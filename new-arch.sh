cb36e58c6e6bb0a0758d6f24511517c4eab45be7
urthbound <jeffowler@gmail.com>
2016-07-17 17:38:55 -0400

    new arch no x

--- new-arch.sh
+++ new-arch.sh
@@ -1,59 +1,69 @@
 #!/bin/bash
 
-# DO NOT RUN THIS unless you want /dev/sda to be completely destroyed.
+echo -n "Are you sure? This will destroy and recreate /dev/sda\n yes/no?"
+read response
+echo
 
-umount /mnt/boot
-umount /mnt
-swapoff -a
+if [[ $response =~ ^[Yy]es$ ]]
+then
 
-parted -s /dev/sda mklabel gpt
+    # make sure nothing is mounted and turn off all swap partitions
+    umount /mnt/boot
+    umount /mnt
+    swapoff -a
 
-parted -s /dev/sda mkpart msdos 2048s 411647s
-parted -s /dev/sda set 1 esp on
-parted -s /dev/sda set 1 boot on
-mkfs.fat -F32 /dev/sda1 -F
+    # create guid partition table
+    parted -s /dev/sda mklabel gpt
 
-parted -s /dev/sda mkpart ext4 411648s 42354687s
-mkfs.ext4 /dev/sda2 -F
+    # write /dev/sda1 as an EFI boot partition (msdos + boot flags) into the gpt
+    #                               200MB
+    parted -s /dev/sda mkpart msdos 2048s 411647s
+    parted -s /dev/sda set 1 esp on # efi system partition
+    parted -s /dev/sda set 1 boot on
+    # actually make the efi filesystem
+    mkfs.fat -F32 /dev/sda1 -F
 
-parted -s /dev/sda mkpart linux-swap 42354688s 46548991s
-mkswap /dev/sda3
-swapon /dev/sda3
+    # write /dev/sda2 as an EFI boot partition (msdos + boot flags) into the gpt
+    #                              20GB
+    parted -s /dev/sda mkpart ext4 411648s 42354687s
+    # actually make the ext4
+    mkfs.ext4 /dev/sda2 -F
 
-mount /dev/sda2 /mnt
-mkdir -p /mnt/boot
-mount /dev/sda1 /mnt/boot
-rm -r /mnt/boot/*
+    # write /dev/sda3 as a linux swap partition
+    #                                    2GB
+    parted -s /dev/sda mkpart linux-swap 42354688s 46548991s
+    mkswap /dev/sda3
+    swapon /dev/sda3
 
-pacstrap /mnt       \
-    base            \
-    base-devel      \
-    grub-efi-x86_64 \
-    os-prober       \
-    efibootmgr      \
-    wpa_supplicant  \
-    wireless_tools  \
-    wpa_actiond     \
-    dialog          \
-    git             \
-    vim             \
-    tmux            \
-    wget            \
-    alsa-utils      \
-    xorg-server xorg-xinit xorg-server-utils \
-    nvidia          \
-    xorg-twm xorg-xclock xterm              \
-    ttf-dejavu      \
-    gnome           \
-    sudo
+    # mount / at /mnt and /boot at /mnt/boot
+    mount /dev/sda2 /mnt
+    mkdir -p /mnt/boot
+    mount /dev/sda1 /mnt/boot
+    #remove any residual images in /boot
+    rm -r /mnt/boot/*
 
-genfstab /mnt > /mnt/etc/fstab
+    # install the indicated packages to the new system
+    pacstrap /mnt       \
+        base            \
+        base-devel      \
+        grub-efi-x86_64 \
+        os-prober       \
+        efibootmgr      \
+        wpa_supplicant  \
+        dialog          \
+        git             \
+        vim             \
+        tmux            \
+        wget            \
+        sudo
+
+    genfstab /mnt > /mnt/etc/fstab
 
 
-arch-chroot /mnt /bin/bash -c <<EOF
+    arch-chroot /mnt /bin/bash -c <<EOF
 
-git config --global user.name "Jeff Fowler" 
-git config --global user.email "jeffowler@gmail.com" 
+git config --global user.name "Jeff Fowler"
+git config --global user.email "jeffowler@gmail.com"
 
 echo LANG=en_US.UTF-8 UTF-8 > /etc/locale.conf && export LANG=en_US.UTF-8
 grub-install --efi-directory=/boot && grub-mkconfig -o /boot/grub/grub.cfg
@@ -65,4 +75,6 @@ useradd -m -g users -G wheel,storage,power -s /bin/bash jfo && passwd jfo
 
 EOF
 
-echo "new-arch.sh is done. you can probably reboot now." 
+    echo "new-arch.sh is done. you can probably reboot now."
+
+fi
